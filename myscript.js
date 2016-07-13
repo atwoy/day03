a012c25b65a0eb4f87fc0f0cf3d9d27b82c94470
atwoy <1116at@gmail.com>
2016-07-13 16:05:37 -0700

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,45 @@
+console.log("Starting with example");
+
+function addNumbers(a, b)
+{
+    console.log(a + b)
+}
+
+console.log("Done with example.")
+
+var numberOfMen = 30;
+var numberOfWomen = 10;
+
+addNumbers(numberOfMen, numberOfWomen);
+console.log("Starting with example");
+
+function addNames(var01, var02)
+{
+    var fullName = var01 + ' ' + var02;
+   console.log(fullName);
+}
+
+var firstName = 'Alex';
+var lastName = 'Twoy';
+
+addNames(firstName, lastName);
+
+function addNames(var01, var02)
+{
+    var fullName = var01 + ' ' + var02;
+   console.log(fullName);
+}
+
+var firstName = 'Alex';
+var lastName = 'Twoy';
+
+addNames(firstName, lastName);
+
+console.log("I'm done.");
+
+var age = 20;
+if (age ≥ 35) 
+{
+    console.log('You can vote AND hold any place in government!');
+} 
+else if (age ≥ 25)
